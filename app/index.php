bbceca6fee25c466776320d64ef45cc90ed3699a
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-11-05 22:31:25 +0200

    curl request response with different API

--- app/index.php
+++ app/index.php
@@ -1,31 +1,57 @@
 <?php
 
-if (!empty($_GET['name'])) {
-    $response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
+$ch = curl_init();
 
-    $data = json_decode($response, true);
+//curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    $age = $data['age'];
-}
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Example</title>
-</head>
-<body>
+// equal to top ^^
 
-<?php
-if (isset($age)) : ?>
+// openweather API
+//curl_setopt_array($ch, [
+//    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2dbe71439a120047ce40a4f02931f233",
+//    CURLOPT_RETURNTRANSFER => true,
+//    CURLOPT_HEADER => true
+//]);
 
-    Age: <?= $age ?>
+$headers = [
+    "Authorization: Client-ID pmFLrgAYVSsNPVSEBldWN_218FU2URPHEE8hOi3br7U"
+];
 
-<?php endif; ?>
-<form>
-    <label for="name">Name</label>
-    <input name="name" id="name">
+$response_headers = [];
 
-    <button>Guess age</button>
-</form>
-</body>
-</html>
+$header_callback = function ($ch, $header) use (&$response_headers) {
+    $len = strlen($header);
+
+    $parts = explode(":", $header, 2);
+
+    if (count($parts) < 2) {
+        return $len;
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+};
+
+// unsplash API
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_HEADERFUNCTION => $header_callback,
+]);
+
+$response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+echo $status_code, "<br>";
+
+print_r($response_headers);
+
+echo "<pre>";
+echo $response;
+echo "<pre>";
