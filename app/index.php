b15e2454017c03731054ee9a3ce789df9713a6d8
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-11-05 23:18:23 +0200

    star unstar github repo using API

--- app/index.php
+++ app/index.php
@@ -2,44 +2,19 @@
 
 $ch = curl_init();
 
-//curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// equal to top ^^
-
-// openweather API
-//curl_setopt_array($ch, [
-//    CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2dbe71439a120047ce40a4f02931f233",
-//    CURLOPT_RETURNTRANSFER => true,
-//    CURLOPT_HEADER => true
-//]);
-
 $headers = [
-    "Authorization: Client-ID pmFLrgAYVSsNPVSEBldWN_218FU2URPHEE8hOi3br7U"
+    "Authorization: Bearer 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",
+//    "User-Agent : Smash2222" here or in const
 ];
 
-$response_headers = [];
-
-$header_callback = function ($ch, $header) use (&$response_headers) {
-    $len = strlen($header);
-
-    $parts = explode(":", $header, 2);
-
-    if (count($parts) < 2) {
-        return $len;
-    }
-
-    $response_headers[$parts[0]] = trim($parts[1]);
-
-    return $len;
-};
-
-// unsplash API
+// github API
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
-    CURLOPT_HEADERFUNCTION => $header_callback,
+    CURLOPT_USERAGENT => "Smash2222",
+    CURLOPT_CUSTOMREQUEST => "PUT"
+//    CURLOPT_CUSTOMREQUEST => "DELETE" to unstar repo
 ]);
 
 $response = curl_exec($ch);
@@ -49,8 +24,6 @@ $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 echo $status_code, "<br>";
-
-print_r($response_headers);
 
 echo "<pre>";
 echo $response;
