1fb1f9501500dbc5b3eb6f8e8e2967d80b2dff8e
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-11-05 23:33:24 +0200

    create repo using API

--- app/index.php
+++ app/index.php
@@ -3,18 +3,24 @@
 $ch = curl_init();
 
 $headers = [
-    "Authorization: Bearer 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",
+    "Authorization: Bearer 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",
 //    "User-Agent : Smash2222" here or in const
 ];
 
+$payload = json_encode([
+    "name" => "Created from API",
+    "description" => "an example API-created repo"
+]);
+
 // github API
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",
+    CURLOPT_URL => "https://api.github.com/user/repos",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTPHEADER => $headers,
     CURLOPT_USERAGENT => "Smash2222",
-    CURLOPT_CUSTOMREQUEST => "PUT"
-//    CURLOPT_CUSTOMREQUEST => "DELETE" to unstar repo
+//    CURLOPT_CUSTOMREQUEST => "POST",
+//    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $payload
 ]);
 
 $response = curl_exec($ch);
