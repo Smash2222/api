636e3de17c3d885a6d8697fd9b3a17ce83666d2d
Smash2222 <karbovskyi.volodymyr@gmail.com>
2022-11-06 22:06:15 +0200

    access f RESTful API with cURL

--- app/index.php
+++ app/index.php
@@ -1,36 +1,26 @@
 <?php
 
+//require __DIR__ . "../vendor/autoload.php";
+
 $ch = curl_init();
 
-$headers = [
-    "Authorization: Bearer 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",
-//    "User-Agent : Smash2222" here or in const
-];
-
-$payload = json_encode([
-    "name" => "Created from API",
-    "description" => "an example API-created repo"
-]);
-
-// github API
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.github.com/user/repos",
+    CURLOPT_URL => "https://api.github.com/gists/917287255448d1e018306be7be7c0968",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers,
     CURLOPT_USERAGENT => "Smash2222",
-//    CURLOPT_CUSTOMREQUEST => "POST",
-//    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => $payload
 ]);
 
 $response = curl_exec($ch);
 
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
 curl_close($ch);
 
-echo $status_code, "<br>";
+$data = json_decode($response, true);
 
-echo "<pre>";
-echo $response;
-echo "<pre>";
+echo '<pre>';
+print_r($data);
+echo '<pre>';
+
+//foreach ($data as $gist) {
+//
+//    echo $gist['id'], " - ", $gist['description'], '<br>';
+//}
